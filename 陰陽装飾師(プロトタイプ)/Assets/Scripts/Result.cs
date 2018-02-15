a64e4bd215f11f2f533dc2b403127700e46548f6
Furukawa-Masaru <30943698+Furukawa-Masaru@users.noreply.github.com>
2018-02-15 18:41:25 +0900

    test

---
+++ 陰陽装飾師(プロトタイプ)/Assets/Scripts/Result.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Result : MonoBehaviour {
+
+    public Image Condition;
+    public GameObject[] Shikigami = new GameObject[5];
+    public GameObject[] Youkai = new GameObject[5];
+
+    // Use this for initialization
+    void Start () {
+
+        GameObject DataManager = GameObject.Find("DataManager");
+
+        if (DataManager.GetComponent<DataManager>().read_win() == true)
+        {
+            Condition.sprite = Resources.Load<Sprite>("Effect/win");
+            DataManager.GetComponent<AudioSource>().PlayOneShot(Resources.Load<AudioClip>("Sound/SE/win"));
+            Youkai[DataManager.GetComponent<DataManager>().read_advaice_mode()].GetComponent<RectTransform>().localScale = new Vector3(0.6f, 0.6f, 1);
+        }
+        else
+        {
+            Condition.sprite = Resources.Load<Sprite>("Effect/lose");
+            DataManager.GetComponent<AudioSource>().PlayOneShot(Resources.Load<AudioClip>("Sound/SE/lose"));
+            Shikigami[DataManager.GetComponent<DataManager>().read_advaice_mode()].GetComponent<RectTransform>().localScale = new Vector3(0.6f, 0.6f, 1);
+        }
+
+        Shikigami[DataManager.GetComponent<DataManager>().read_advaice_mode()].SetActive(true);
+        Youkai[DataManager.GetComponent<DataManager>().read_advaice_mode()].SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void To_Advice()
+    {
+        Button_Clicked();
+    
+        StartCoroutine(Sample("Advice"));
+    }
+
+    public void Button_Clicked()
+    {
+        GameObject.Find("DataManager").GetComponent<AudioSource>().PlayOneShot(Resources.Load<AudioClip>("Sound/SE/Button_Click"));
+    }
+
+    // コルーチン  
+    private IEnumerator Sample(string name)
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        SceneManager.LoadScene(name);
+    }
+}
---
+++ 陰陽装飾師(プロトタイプ)/Assets/Scripts/Select.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Select : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void To_Title()
+    {
+        StartCoroutine(Load_Scene("Title"));
+    }
+
+    // コルーチン  
+    private IEnumerator Load_Scene(string name)
+    {
+        GameObject.Find("DataManager").GetComponent<AudioSource>().PlayOneShot(Resources.Load<AudioClip>("Sound/SE/Button_Click"));
+
+        yield return new WaitForSeconds(2.0f);
+
+        SceneManager.LoadScene(name);
+    }
+
+}
